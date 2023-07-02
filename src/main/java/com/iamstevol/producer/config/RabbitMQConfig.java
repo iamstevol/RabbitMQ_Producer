34795287cb9d18d34a3c7fc894b791a6c1bd42e1
iamstevol <stevol2015@gmail.com>
2023-07-02 16:02:37 +0100

    Initializing a connection between the rabbitMQ server and the producer

---
+++ src/main/java/com/iamstevol/producer/config/RabbitMQConfig.java
@@ -0,0 +1,16 @@
+package com.iamstevol.producer.config;
+
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.context.annotation.Bean;
+
+public class RabbitMQConfig {
+
+    @Bean
+    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+
+        return container;
+    }
+}
---
+++ src/test/java/com/iamstevol/producer/ProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iamstevol.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
