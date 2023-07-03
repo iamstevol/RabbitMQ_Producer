cbc3df83ec4643d93a3ae3e346355f4a2aa818e0
iamstevol <stevol2015@gmail.com>
2023-07-03 08:49:13 +0100

    Configured the java object and rabbitMQ to communicate together

--- src/main/java/com/iamstevol/producer/config/RabbitMQConfig.java
+++ src/main/java/com/iamstevol/producer/config/RabbitMQConfig.java
@@ -3,7 +3,9 @@ package com.iamstevol.producer.config;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class RabbitMQConfig {
 
     @Bean
